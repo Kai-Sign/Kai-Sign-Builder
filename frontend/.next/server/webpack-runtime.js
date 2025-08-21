337018afeed74b4480207641f082e9fb77f503c8
Nathanael Richard Ha Hanes <natrichardha@gmail.com>
2025-08-21 23:40:45 +0800

    feat: update app and server manifests with new routes and build artifacts
    - Added new routes for the main page and health check API to app-build-manifest.json and app-paths-manifest.json.
    - Updated webpack runtime hash in server files for improved build consistency.
    - Removed obsolete cached images and hot-update files to clean up the build output.
    - Enhanced the webpack configuration to streamline chunk loading and optimize performance.

--- frontend/.next/server/webpack-runtime.js
+++ frontend/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("5c9b56c582e0f8c6")
+/******/ 		__webpack_require__.h = () => ("3241b6d19d46f2f8")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
@@ -163,6 +163,11 @@
 /******/ 			var r = fn();
 /******/ 			return r === undefined ? result : r;
 /******/ 		}
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	(() => {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/require chunk loading */
